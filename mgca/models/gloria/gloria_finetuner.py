3a060a2247c19eabaee50e5855f3bdd0a4b5a868
tzcskys <lmjaubd@gmail.com>
2023-07-11 22:02:22 +1000

    evaluate with my pre-trained gloria model

---
+++ mgca/models/gloria/gloria_finetuner.py
@@ -0,0 +1,377 @@
+import os
+import torch
+from argparse import ArgumentParser
+from dateutil import tz
+import datetime
+from pytorch_lightning import seed_everything, Trainer
+from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.callbacks import (
+    EarlyStopping,
+    LearningRateMonitor,
+    ModelCheckpoint,
+    StochasticWeightAveraging
+)
+from mgca.datasets.data_module import DataModule
+from mgca.datasets.classification_dataset import CheXpertImageDataset, RSNAImageDataset, COVIDXImageDataset
+from mgca.datasets.transforms import DataTransforms, Moco2Transform
+from mgca.models.ssl_finetuner import SSLFineTuner
+from mgca.models.gloria.gloria_module import GLoRIA
+
+import re
+import hashlib
+import urllib
+import warnings
+import tqdm
+import torch.nn.functional as F
+from torch import nn
+from collections import OrderedDict
+
+
+torch.autograd.set_detect_anomaly(True)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = True
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+_MODELS = {
+    "RN50": "https://openaipublic.azureedge.net/clip/models/afeb0e10f9e5a86da6080e35cf09123aca3b358a0c3e3b6c78a7b63bc04b6762/RN50.pt",
+    "RN101": "https://openaipublic.azureedge.net/clip/models/8fa8567bab74a42d41c5915025a8e4538c3bdbe8804a470a72f30b0d94fab599/RN101.pt",
+    "RN50x4": "https://openaipublic.azureedge.net/clip/models/7e526bd135e493cef0776de27d5f42653e6b4c8bf9e0f653bb11773263205fdd/RN50x4.pt",
+    "RN50x16": "https://openaipublic.azureedge.net/clip/models/52378b407f34354e150460fe41077663dd5b39c54cd0bfd2b27167a4a06ec9aa/RN50x16.pt",
+    "RN50x64": "https://openaipublic.azureedge.net/clip/models/be1cfb55d75a9666199fb2206c106743da0f6468c9d327f3e0d0a543a9919d9c/RN50x64.pt",
+    "ViT-B/32": "https://openaipublic.azureedge.net/clip/models/40d365715913c9da98579312b702a82c18be219cc2a73407c4526f58eba950af/ViT-B-32.pt",
+    "ViT-B/16": "https://openaipublic.azureedge.net/clip/models/5806e77cd80f8b59890b7e101eabd078d9fb84e6937f9e85e4ecb61988df416f/ViT-B-16.pt",
+    "ViT-L/14": "https://openaipublic.azureedge.net/clip/models/b8cca3fd41ae0c99ba7e8951adf17d267cdb84cd88be6f7c2e0eca1737a03836/ViT-L-14.pt",
+    "ViT-L/14@336px": "https://openaipublic.azureedge.net/clip/models/3035c92b350959924f9f00213499208652fc7ea050643e8b385c2dac08641f02/ViT-L-14-336px.pt",
+}
+
+def _download(url: str, root: str = os.path.expanduser("~/.cache/clip")):
+    os.makedirs(root, exist_ok=True)
+    filename = os.path.basename(url)
+    expected_sha256 = url.split("/")[-2]
+    download_target = os.path.join(root, filename)
+    if os.path.exists(download_target) and not os.path.isfile(download_target):
+        raise RuntimeError(f"{download_target} exists and is not a regular file")
+    if os.path.isfile(download_target):
+        if hashlib.sha256(open(download_target, "rb").read()).hexdigest() == expected_sha256:
+            return download_target
+        else:
+            warnings.warn(f"{download_target} exists, but the SHA256 checksum does not match; re-downloading the file")
+    with urllib.request.urlopen(url) as source, open(download_target, "wb") as output:
+        with tqdm(total=int(source.info().get("Content-Length")), ncols=80, unit='iB', unit_scale=True) as loop:
+            while True:
+                buffer = source.read(8192)
+                if not buffer:
+                    break
+                output.write(buffer)
+                loop.update(len(buffer))
+    if hashlib.sha256(open(download_target, "rb").read()).hexdigest() != expected_sha256:
+        raise RuntimeError(f"Model has been downloaded but the SHA256 checksum does not not match")
+    return download_target
+
+def adapt_position_encoding(model, patch_size=32, after=384, suffix='visual.positional_embedding'):
+    keys = [k for k in model if k.endswith(suffix)]
+    assert len(keys) == 1
+    key = keys[0]
+    origin_pos_embed = model[key]
+    origin_dim2 = False
+    if len(origin_pos_embed.shape) == 2:
+        origin_dim2 = True
+        origin_pos_embed = origin_pos_embed.unsqueeze(0)
+    grid_before = int(np.sqrt(origin_pos_embed.shape[1] - 1))
+    before = int(grid_before * patch_size)
+    assert (before % patch_size) == 0
+    grid_after = after // patch_size
+    assert (after % patch_size) == 0
+    embed_dim = origin_pos_embed.shape[-1]
+
+    pos_embed = origin_pos_embed[0, 1:, :].reshape((grid_before, grid_before, embed_dim))
+    new_size = (grid_after, grid_after)
+    pos_embed = torch.nn.functional.interpolate(pos_embed.permute((2, 0, 1)).unsqueeze(0), size=new_size,
+                                                mode='bicubic')
+    pos_embed = pos_embed.squeeze(0).permute((1, 2, 0)).reshape((-1, embed_dim))
+    pos_embed = torch.cat((origin_pos_embed[0, 0:1, :], pos_embed), dim=0).unsqueeze(0)
+    assert pos_embed.shape == (1, grid_after * grid_after + 1, embed_dim)
+    if origin_dim2:
+        assert pos_embed.shape[0] == 1
+        pos_embed = pos_embed.squeeze(0)
+    model[key] = pos_embed
+    return model
+
+def available_models():
+    """Returns the names of available CLIP models"""
+    return list(_MODELS.keys())
+
+class LayerNorm(nn.LayerNorm):
+    """Subclass torch's LayerNorm to handle fp16."""
+
+    def forward(self, x: torch.Tensor):
+        orig_type = x.dtype
+        ret = super().forward(x.type(torch.float32))
+        return ret.type(orig_type)
+
+
+class QuickGELU(nn.Module):
+    def forward(self, x: torch.Tensor):
+        return x * torch.sigmoid(1.702 * x)
+
+
+class ResidualAttentionBlock(nn.Module):
+    def __init__(self, d_model: int, n_head: int, attn_mask: torch.Tensor = None):
+        super().__init__()
+        self.attn = nn.MultiheadAttention(d_model, n_head)
+        self.ln_1 = LayerNorm(d_model)
+        self.mlp = nn.Sequential(OrderedDict([
+            ("c_fc", nn.Linear(d_model, d_model * 4)),
+            ("gelu", QuickGELU()),
+            ("c_proj", nn.Linear(d_model * 4, d_model))
+        ]))
+        self.ln_2 = LayerNorm(d_model)
+        self.attn_mask = attn_mask
+
+    def attention(self, x: torch.Tensor, x_mask: torch.Tensor):
+        if x_mask is not None:
+            x_mask = x_mask.to(dtype=torch.bool, device=x.device)
+        self.attn_mask = self.attn_mask.to(dtype=x.dtype, device=x.device) if self.attn_mask is not None else None
+        return self.attn(x, x, x, need_weights=False, attn_mask=self.attn_mask, key_padding_mask=x_mask)[0]
+
+    def forward(self, x: torch.Tensor, x_mask: torch.Tensor = None):
+        x = x + self.attention(self.ln_1(x), x_mask)
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
+
+class Transformer(nn.Module):
+    def __init__(self, width: int, layers: int, heads: int, attn_mask: torch.Tensor = None):
+        super().__init__()
+        self.width = width
+        self.layers = layers
+        self.resblocks = nn.Sequential(*[ResidualAttentionBlock(width, heads, attn_mask) for _ in range(layers - 1)])
+
+    def forward(self, x: torch.Tensor, x_mask: torch.Tensor = None):
+        for block in self.resblocks:
+            x = block(x, x_mask)
+        return x
+
+
+class VisualTransformer(nn.Module):
+    def __init__(self, input_resolution: int, patch_size: int, width: int, layers: int, heads: int, output_dim: int,
+                 resolution_after: int):
+        super().__init__()
+        self.input_resolution = input_resolution
+        self.output_dim = output_dim
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=width, kernel_size=patch_size, stride=patch_size, bias=False)
+        scale = width ** -0.5
+        self.class_embedding = nn.Parameter(scale * torch.randn(width))
+        self.positional_embedding = nn.Parameter(scale * torch.randn((resolution_after // patch_size) ** 2 + 1, width))
+        self.ln_pre = LayerNorm(width)
+        self.transformer = Transformer(width, layers, heads)
+        self.ln_post = LayerNorm(width)
+
+        self.width = width
+
+    def forward(self, x: torch.Tensor, x_mask=None):
+        x = self.conv1(x)  # shape = [*, width, grid, grid]
+        x = x.reshape(x.shape[0], x.shape[1], -1)  # shape = [*, width, grid ** 2]
+        x = x.permute(0, 2, 1)  # shape = [*, grid ** 2, width]
+        t = self.class_embedding.to(x.dtype) + torch.zeros(x.shape[0], 1, x.shape[-1], dtype=x.dtype, device=x.device)
+        x = torch.cat([t, x], dim=1)  # shape = [*, grid ** 2 + 1, width]
+        x = x + self.positional_embedding.to(x.dtype)
+        x = self.ln_pre(x)
+
+        x = x.permute(1, 0, 2)  # NLD -> LND
+        x = self.transformer(x, x_mask)
+        x = x.permute(1, 0, 2)  # LND -> NLD
+
+        x = self.ln_post(x)
+
+        # return x
+        return x[:, 0, :], x[:, 1:, :]  # this is just for fine-tuning in MGCA project!!
+
+    def forward_patch_embed(self, x: torch.Tensor, x_mask): # width here is like the feature dim, this function is to get the patch embedding
+        x = self.conv1(x)  # shape = [*, width, grid, grid]
+        x = x.reshape(x.shape[0], x.shape[1], -1)  # shape = [*, width, grid ** 2]
+        x = x.permute(0, 2, 1)  # shape = [*, grid ** 2, width]
+        t = self.class_embedding.to(x.dtype) + torch.zeros(x.shape[0], 1, x.shape[-1], dtype=x.dtype, device=x.device)
+        x = torch.cat([t, x], dim=1)  # shape = [*, grid ** 2 + 1, width]
+        return x
+
+    def forward_pos_embed(self, x: torch.Tensor, x_mask):
+        x = x + self.positional_embedding.to(x.dtype)
+        return x
+
+    def forward_trans(self, x: torch.Tensor, x_mask):
+        x = self.ln_pre(x)
+        x = x.permute(1, 0, 2)  # NLD -> LND
+        x = self.transformer(x, x_mask)
+        x = x.permute(1, 0, 2)  # LND -> NLD
+        x = self.ln_post(x)
+        return x
+
+
+
+def build_model(name, resolution_after=224, jit=False):
+    if name in _MODELS:
+        model_path = _download(_MODELS[name])
+    elif os.path.isfile(name):
+        model_path = name
+    else:
+        raise RuntimeError(f"Model {name} not found; available models = {available_models()}"
+                           )
+    try:
+        model_clip = torch.jit.load(model_path, map_location="cpu")
+        state_dict = None
+    except RuntimeError:
+        if jit:
+            warnings.warn(f"File {model_path} is not a JIT archive. Loading as a state dict instead")
+            jit = False
+        state_dict = torch.load(model_path, map_location="cpu")
+    state_dict = state_dict or model_clip.state_dict()
+
+    vision_width = state_dict["visual.conv1.weight"].shape[0]
+    vision_layers = len(
+        [k for k in state_dict.keys() if k.startswith("visual.") and k.endswith(".attn.in_proj_weight")])
+    vision_patch_size = state_dict["visual.conv1.weight"].shape[-1]
+    grid_size = round((state_dict["visual.positional_embedding"].shape[0] - 1) ** 0.5)
+    image_resolution = vision_patch_size * grid_size
+    embed_dim = state_dict["text_projection"].shape[1]
+    vision_heads = vision_width // 64
+
+    model = VisualTransformer(
+            input_resolution=image_resolution,
+            patch_size=vision_patch_size,
+            width=vision_width,
+            layers=vision_layers,
+            heads=vision_heads,
+            output_dim=embed_dim, # no use, this is for text
+            resolution_after=resolution_after,
+    )
+
+    # for key in ["input_resolution", "context_length", "vocab_size"]:
+    #     if key in state_dict:
+    #         del state_dict[key]
+    #
+    # model_dict = model.state_dict()
+    # pretrained_dict = state_dict
+    # if resolution_after != image_resolution:
+    #     pretrained_dict = adapt_position_encoding(pretrained_dict, after=resolution_after, patch_size=vision_patch_size)
+    # # 1. filter out unnecessary keys
+    # pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+    # # 2. overwrite entries in the existing state dict
+    # model_dict.update(pretrained_dict)
+    # # 3. load the new state dict
+    # model.load_state_dict(model_dict)
+    return model
+
+def cli_main():
+    parser = ArgumentParser()
+    parser.add_argument("--dataset", type=str, default="chexpert")
+    parser.add_argument("--path", type=str,
+                        default="/mnt/HDD2/mingjian/results/pre_trained_model/mgca/6.162008_15_347547.pth")
+    parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--batch_size", type=int, default=48)
+    parser.add_argument("--num_workers", type=int, default=16)
+    parser.add_argument("--data_pct", type=float, default=0.01)
+    # add trainer args
+    parser = Trainer.add_argparse_args(parser)
+    args = parser.parse_args()
+
+    # set max epochs
+    args.max_epochs = 50
+
+    seed_everything(args.seed)
+
+    if args.dataset == "chexpert":
+        # define datamodule
+        # check transform here
+        datamodule = DataModule(CheXpertImageDataset, None,
+                                Moco2Transform, args.data_pct,
+                                args.batch_size, args.num_workers)
+        num_classes = 5
+        multilabel = True
+    elif args.dataset == "rsna":
+        datamodule = DataModule(RSNAImageDataset, None,
+                                DataTransforms, args.data_pct,
+                                args.batch_size, args.num_workers)
+        num_classes = 1
+        multilabel = True
+    elif args.dataset == "covidx":
+        datamodule = DataModule(COVIDXImageDataset, None,
+                                DataTransforms, args.data_pct,
+                                args.batch_size, args.num_workers)
+        num_classes = 3
+        multilabel = False
+    else:
+        raise RuntimeError(f"no dataset called {args.dataset}")
+
+
+    if args.path:
+        # model = GLoRIA.load_from_checkpoint(args.path, strict=False)
+        model = build_model("ViT-B/32")
+
+        state_dict = torch.load(args.path)
+        # the vision_encoder.ln_final, vision_encoder.token_embedding, vision_encoder.positional_embedding are not used since it is used by m3ae for text, no relation to the img
+        # but the vision_encoder.visual.positional_embedding is used
+        params = {re.sub('^vision_encoder.visual.', '', k): v for k, v in state_dict["model"].items()}
+        params = {k: v for k, v in params.items() if k in model.state_dict()}
+        model.load_state_dict(params, strict=True)
+    else:
+        # model = GLoRIA()
+        raise RuntimeError("no path provided")
+
+    # args.model_name = model.hparams.img_encoder
+    # args.backbone = model.img_encoder
+    # args.in_features = args.backbone.feature_dim
+
+    args.model_name = "vit_m3ae_version"
+    args.backbone = model # TODO
+    args.in_features = args.backbone.width # TODO
+
+    args.num_classes = num_classes
+    args.multilabel = multilabel
+
+
+    # finetune
+    tuner = SSLFineTuner(**args.__dict__)
+
+    # get current time
+    now = datetime.datetime.now(tz.tzlocal())
+    extension = now.strftime("%Y_%m_%d_%H_%M_%S")
+    ckpt_dir = os.path.join(
+        BASE_DIR, f"../../../data/ckpts/gloria_finetune/{extension}")
+    os.makedirs(ckpt_dir, exist_ok=True)
+    callbacks = [
+        LearningRateMonitor(logging_interval="step"),
+        ModelCheckpoint(monitor="val_loss", dirpath=ckpt_dir,
+                        save_last=True, mode="min", save_top_k=1),
+        EarlyStopping(monitor="val_loss", min_delta=0.,
+                      patience=10, verbose=False, mode="min")
+    ]
+
+    # get current time
+    now = datetime.datetime.now(tz.tzlocal())
+
+    extension = now.strftime("%Y_%m_%d_%H_%M_%S")
+    logger_dir = os.path.join(
+        BASE_DIR, f"../../../data/wandb")
+    os.makedirs(logger_dir, exist_ok=True)
+    wandb_logger = WandbLogger(
+        project="gloria_finetune",
+        save_dir=logger_dir, 
+        name=f"{args.dataset}_{args.data_pct}_{extension}")
+    trainer = Trainer.from_argparse_args(
+        args,
+        deterministic=True,
+        callbacks=callbacks,
+        logger=wandb_logger)
+
+    tuner.training_steps = tuner.num_training_steps(trainer, datamodule)
+
+    # train
+    trainer.fit(tuner, datamodule)
+    # test
+    trainer.test(tuner, datamodule, ckpt_path="best")
+
+
+if __name__ == "__main__":
+    cli_main()
