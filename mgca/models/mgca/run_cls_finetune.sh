b676afd75d5a75cc1bd4b7be9ab1a88cda3e2f59
tzcskys <lmjaubd@gmail.com>
2023-10-04 17:23:30 +1100

    script for linear cls and seg

---
+++ mgca/models/mgca/run_cls_finetune.sh
@@ -0,0 +1,7 @@
+#CUDA_VISIBLE_DEVICES=1 python mgca_finetuner.py --gpus 1 --dataset chexpert --data_pct 0.01
+#CUDA_VISIBLE_DEVICES=1 python mgca_finetuner.py --gpus 1 --dataset chexpert --data_pct 0.1
+CUDA_VISIBLE_DEVICES=1 python mgca_finetuner.py --gpus 1 --dataset chexpert --data_pct 1 --batch_size 96
+
+#CUDA_VISIBLE_DEVICES=1 python mgca_finetuner.py --gpus 1 --dataset rsna --data_pct 0.01
+#CUDA_VISIBLE_DEVICES=1 python mgca_finetuner.py --gpus 1 --dataset rsna --data_pct 0.1
+CUDA_VISIBLE_DEVICES=1 python mgca_finetuner.py --gpus 1 --dataset rsna --data_pct 1
